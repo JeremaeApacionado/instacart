0730980aca9bb82b33d86f44b780f5a5a835ab5e
Jeremae Apacionado <jtapacionado1998@gmail.com>
2022-05-05 14:47:00 +0800

    last commit successful

--- models/user.go
+++ models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	ID        uint   `json:"id" gorm:"foreignKey"`
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lastname"`
-	Email     string `json:"email"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
+	ID        	  uint   `json:"id" gorm:"foreignKey"`
+	Name 		  string `json:"name"`
+	Email   	  string `json:"email"`
+	Address		  string `json:"address"`
+	Username	  string `json:"username"`
+	Password  	  string `json:"password"`
 }
