f92f29e75f5fca6d2ad688aabe5cbd4e79c4a972
Jeremae Apacionado <jtapacionado1998@gmail.com>
2022-05-11 16:41:15 +0800

    lowercase

--- models/user.go
+++ models/user.go
@@ -2,9 +2,9 @@ package models
 
 type User struct {
 	UserID       uint   `json:"id" gorm:"primaryKey; autoIncrement"`
-	Fullname 	  string `json:"Fullname"`
-	Email   	  string `json:"Email"`
-	Address		  string `json:"Address"`
-	Username	  string `json:"Username"`
-	Password  	  string `json:"Password"`
+	fullname 	  string `json:"fullname"`
+	email   	  string `json:"email"`
+	address		  string `json:"address"`
+	username	  string `json:"username"`
+	password  	  string `json:"password"`
 }
